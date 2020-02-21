6e4943733e9a891bc00ac86590935ce434f89ac5
perrishnikov <perrishnikov@gmail.com>
2020-02-21 15:42:39 -0600

    moved to https

--- popup.js
+++ popup.js
@@ -26,7 +26,8 @@ const handle = {
   link: document.querySelector('#linkButt'),
   reset: document.querySelector('#reset'),
   out: document.querySelector('#output-select'),
-  message: document.querySelector('#message')
+  message: document.querySelector('#message'),
+  debug: document.querySelector('#debug'),
   // pg: document.querySelector('#pages-select'),
   // secondary: document.querySelector('#secondary')
 };
@@ -53,13 +54,21 @@ const initialize = () => {
 
 /**
  * Slices up the Salsify tab's url
+ * sample url:  http://images.salsify.com/image/upload/s--qT0Rpe-g--/hc4jqgdybrvlb5hfmo5a
  * @param {string} url 
  * @returns {{first:string, last:string}}
  */
-const breakUrl = url => {
+const breakApartUrl = url => {
   const split = url.split('/');
-  const first = split.slice(0, split.length - 1).join('/');
-  const last = split[split.length - 1].split('.')[0];
+  let [protocol] = [...split].slice(0, 1); //copy and strip array
+  const rest = [...split].slice(1, split.length - 1).join('/');
+  const last = [...split][split.length - 1].split('.')[0];
+
+  if (protocol === 'http:') {
+    protocol = 'https:';
+  }
+
+  const first = [protocol, rest].join('/');
 
   return { first, last };
 };
@@ -75,6 +84,28 @@ const rebuildUrl = obj => {
 };
 
 
+const getPermission = () => {
+  /** 
+   * Get permission...
+   * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard
+  */
+  navigator.permissions.query({ name: 'clipboard-write' })
+    .then(result => {
+      console.log('result: ', result.state);
+      if (result.state == 'granted' ||
+        result.state == 'prompt') {
+
+        /* write to the clipboard now */
+        //! addTextClipboard(goodUrl);
+
+      } else {
+        alert('Permissions issue. See Perry');
+      }
+    });
+
+};
+
+
 /**
  * Wrtite Text to Clipboard
  * @param {string} newClip 
@@ -87,7 +118,7 @@ const addTextClipboard = newClip => {
     handle.link.textContent = 'COPIED!';
     handle.link.classList = 'copied';
 
-    /** Close popup to help Users get on with their lives */
+    /** Close popup to help Users get on with their lives; window is popup */
     setTimeout(() => {
       window.close();
     }, 2000);
@@ -184,45 +215,51 @@ handle.image.onclick = function () {
 /** 
  * Sets the event listener for primary button
 */
-handle.link.onclick = function () {
+handle.link.onclick = () => {
 
-  chrome.tabs.query({ 'active': true }, (tabs) => {
-    //http://images.salsify.com/image/upload/s--qT0Rpe-g--/hc4jqgdybrvlb5hfmo5a
-    const url = tabs[0].url;
-    const brokenUrl = breakUrl(url);
-    const goodUrl = rebuildUrl(brokenUrl);
+  chrome.tabs.query({ 'active': true }, tabs => {
+    const brokenUrl = breakApartUrl(tabs[0].url);
+    const rebuiltUrl = rebuildUrl(brokenUrl);
+    const tabId = tabs[0].id;
+
+
+    /** add listener to caputure completed https request.... */
+    // eslint-disable-next-line no-shadow
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, newTab) => {
+
+      if (changeInfo.status === 'complete') {
+        // alert('job\'s done');
+        /** Now we're in https. Need to query the new https tab and do the work. */
+        chrome.tabs.query({ 'active': true }, httpsTab => {
+          console.log('now on https... Requesting permission from Clipboard API....');
+      
+          //! do the promise here
+          const permission = getPermission();
+          
+        });
+      }
+    });
 
 
     /** 
-     * Get permission...
-     * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard
-    */
-    navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
-      if (result.state == 'granted' ||
-        result.state == 'prompt') {
+     * Request the https version of url.
+     * It is async; we cant get new url in callback;
+     * Need to use the 'onUpdated' listener above
+     */
+    chrome.tabs.update(tabId, { url: rebuiltUrl }, () => {console.log('requesting https...');});
 
-        /* write to the clipboard now */
-        addTextClipboard(goodUrl);
-
-      } else {
-        alert('Permissions issue. See Perry');
-      }
-    });
   });
 };
 
-/** 
- * Sets event listener for reset button
-*/
-// handle.reset.onclick = () => {
-//   console.log('reset');
-//   initialize();
-// };
 
 
 /** Run on popup opening */
 initialize();
 
+
+/** 
+ * Handles the disable on COPY button 
+ */
 handle.out.addEventListener('change', (e) => {
   console.log(e.target.value);
   const i = handle.image;
